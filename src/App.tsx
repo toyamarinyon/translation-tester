731e07075a467d42e758a45174628cb0713b1433
satoshi toyama <toyamarinyon@gmail.com>
2023-01-21 16:24:43 +0900

    easier the example

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const schema = z.object({
 const examples = [
   {
     name: "React(programing)",
-    text: "React is a JavaScript library for rendering user interfaces (UI). UI is built from small units like buttons, text, and images. React lets you combine them into reusable, nestable components. From web sites to phone apps, everything on the screen can be broken down into components.",
+    text: "React is a JavaScript library for rendering user interfaces (UI)",
   },
   {
     name: "DEATH STRANDING(Game)",
